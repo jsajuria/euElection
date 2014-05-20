a3c0b47a570b8a13283feda87c84f5755a735e8e
Javier Sajuria <jsajuria@users.noreply.github.com>
2014-05-20 20:41:29 +0100

    Update subset_polis.R

--- R/subset_polis.R
+++ R/subset_polis.R
@@ -4,7 +4,6 @@
 #' 'europe','ukip','farage','labour','miliband','conservative','cameron','libdem','clegg','lucas','green party','tory','politic','election','EU','parliament','government','pmq','vote','opposition'
 ##' @param df is the dataframe of tweets. The text of the tweets whould be under a column named "text"
 #' @keywords Twitter, subset
-
 #' @examples
 #' subset_polis()
 
